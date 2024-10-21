8ca7f9c361c440a8a7205cc28e21cb257d1a8ddc
heights-m <ryupii.24@gmail.com>
2024-10-21 12:20:49 +0900

    update facility card to dynamic

--- facres_app/src/components/FacilCard.tsx
+++ facres_app/src/components/FacilCard.tsx
@@ -2,29 +2,32 @@ import React from 'react'
 import '/src/components/FacilCard.css'
 
 interface Props {
-    imgLink: string;
     facName: string;
     desc: string;
     days: string;
+    particip: number[];
+    location: string;
+    avail: string;
+    imgSrc: string;
 
 }
 
-function FacilCard() {
+function FacilCard({facName, desc, days, particip, location, avail, imgSrc} : Props) {
   return (
     <div className='bg-white p-3 rounded flex justify-content-center align-items-center m-2' id='fac_card'>
-        <img src="/src/assets/gym.jpg" alt="" className='rounded w-100 mb-4' id='fac_card_img'/>
+        <img src={imgSrc} alt="" className='rounded w-100 mb-4' id='fac_card_img'/>
         <div id='fac_card_details' className=''>
-            <h3>Gym</h3>
-            <p>place used for physical activity</p>
+            <h3>{facName}</h3>
+            <p>{desc}</p>
             <div id="fac_card_desc_box">
                 <svg className='fac-card-svg' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M580-240q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29ZM200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Z"/></svg>
-                <p>Mon, Tue,</p>
+                <p>{days}</p>
                 <svg className='fac-card-svg' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM360-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm400-160q0 66-47 113t-113 47q-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113Z"/></svg>
-                <p>1-5</p>
+                <p>{particip[0]} - {particip[1]}</p>
                 <svg className='fac-card-svg' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 400Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Z"/></svg>
-                <p>A101</p>
+                <p>{location}</p>
                 <svg className='fac-card-svg' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#00000"><path d="M80-160v-112q0-33 17-62t47-44q51-26 115-44t141-18q30 0 58.5 3t55.5 9l-70 70q-11-2-21.5-2H400q-71 0-127.5 17T180-306q-9 5-14.5 14t-5.5 20v32h250l80 80H80Zm542 16L484-282l56-56 82 82 202-202 56 56-258 258ZM400-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm10 240Zm-10-320q33 0 56.5-23.5T480-640q0-33-23.5-56.5T400-720q-33 0-56.5 23.5T320-640q0 33 23.5 56.5T400-560Zm0-80Z"/></svg>
-                <p>Available to all</p>
+                <p>{avail}</p>
             </div>
         </div>
     </div>
