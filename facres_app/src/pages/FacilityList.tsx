39fb5d1245205685869d94471f6f43bf2072829b
heights-m <ryupii.24@gmail.com>
2024-10-22 21:59:33 +0900

    finish facility list page

--- facres_app/src/pages/FacilityList.tsx
+++ facres_app/src/pages/FacilityList.tsx
@@ -5,7 +5,9 @@ import FacData from '../assets/Facility_Data.json'
 function FacilityList() {
   return (
     <div className='px-5 py-2 d-flex justify-content-center container-fluid flex-wrap'>
-      {}
+      {FacData.map((fac) => (
+          <FacilCard facName={fac.Name} desc={fac.Desc} days={fac.Days} particip={fac.Participants} location={fac.Location} avail={fac.Available} imgSrc={fac.Img} />
+      ))}
     </div>
   )
 }
