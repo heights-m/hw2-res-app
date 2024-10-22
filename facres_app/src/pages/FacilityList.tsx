3abf5e97f2fff48d80d7cea80e4d347e32bc8198
heights-m <ryupii.24@gmail.com>
2024-10-22 21:30:47 +0900

    update FacilityList.tsx

--- facres_app/src/pages/FacilityList.tsx
+++ facres_app/src/pages/FacilityList.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import FacilCard from '../components/FacilCard'
+import FacData from '../assets/Facility_Data.json'
 
 function FacilityList() {
   return (
     <div className='px-5 py-2 d-flex justify-content-center container-fluid flex-wrap'>
-      <FacilCard/>
-      <FacilCard/>
+      {}
     </div>
   )
 }
